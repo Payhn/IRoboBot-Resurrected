2231baef12c3f9aa241b491a15a1cfa965595ad9
Payhn <micball94@gmail.com>
2022-03-01 12:46:38 -0600

    Added random dice roller functionality and cleaned up a few things

--- main.py
+++ main.py
@@ -24,9 +24,6 @@ import random
 import discord
 import requests
 
-# from pathlib import Path
-#
-# token = str(Path('.github/workflows/tokens').read_text())
 
 client = discord.Client()
 
@@ -36,7 +33,7 @@ starter_encouragements = ["Cheer up!", "Hang in there!", "You can do it!"]
 
 
 # this is a function to return random quotes from a zenquotes website
-def get_quote():
+def get_quote():  # todo rename this function to zenquote but don't break it. we like it <3
     response = requests.get("https://zenquotes.io/api/random")
     json_data = json.loads(response.text)
     quote = json_data[0]['q'] + " -" + json_data[0]['a'] + " from zenquotes.io"
@@ -68,14 +65,34 @@ async def on_message(message):  # this is what to do if there is a message appea
     if message.author == client.user:
         return  # this will ignore the bot when it's speaking, so it doesn't trigger itself
 
-    msg = message.content
+    msg = message.content  # just shortens message.content to msg so I don't have to type it every time.
 
-    if msg.startswith('$hello'):
+    if msg.startswith('$hello '):
         await message.channel.send("Hello!")
 
-    if msg.startswith('#inspire'):
+    if msg.startswith('#inspire '):
         quote = get_quote()
         await message.channel.send(quote)
+
+    if msg.startswith('@printer '):  # todo, @printer function to print(discord.<variable>)
+        await message.channel.send()
+        '''
+        psudo code
+        user says: @printer User_Input
+        User_Variable = message - "@printer " 
+        print(discord.User_Variable)
+        
+        purpose: to print variables by name while working on the api
+        '''
+
+    if msg.startswith('@random'):  # todo task to pull choose random number from given amount. if no amount given topline defaults to 20
+        # print(message.content)
+        topline = message.content.strip('@random ')
+        # print(topline)
+        if str(topline) == "":
+            topline = 20
+        # print(random.randrange(int(topline)))
+        await message.channel.send("I've rolled a " + str(topline) + " sided die. Your result is, \n" + str(random.randrange(int(topline) + 1)))  # the + 1 is because random starts at 0 and rolling a 6 sided die only gets a max of 5 without this
 
     if any(word in msg for word in
            sad_words):  # todo, need to parse text so that it reads everything lowercase for this check.
@@ -90,7 +107,7 @@ async def on_message(message):  # this is what to do if there is a message appea
 # todo, @sw <input1> <input2> to search the last few chat messages
 
 # todo, twitter search function
-# todo,  @google <input> for in chat googleing
+# todo, @google <input> for in chat googleing
 # todo, @wiki <input> for in chat wiki searching
 
 with open('.env', 'r') as file:  # opens .env file and reads the contents
@@ -98,5 +115,5 @@ with open('.env', 'r') as file:  # opens .env file and reads the contents
     tokenz = file.read().rstrip()
     # todo, need to setup this file so we can read different sections for different tokens.
 
-client.run(tokenz)
+client.run(tokenz)  # this actually starts the bot and passes a password to login from the .env file.
 # client.run('TOKEN-CONTENTS')  # this is the secret token or whatever
